5a2a7501c83330995cfa41a05d0c8f9dcb1315d0
john-pc\John <thewoxyz@gmail.com>
2016-12-25 18:20:21 +0200

    no message

--- dev/js/game/events.js
+++ dev/js/game/events.js
@@ -6,6 +6,12 @@ window.onload = function(){
     var userTank = clone(defaultTank);
     socket.on('join game', function(response){
         var response = JSON.parse(response);
+
+        if(response.status == 'error'){
+            //send back to main menu if no username entered
+            window.location.replace('/');
+        }
+
         clientId = response.userId;
         userTank.userId = response.userId;
         userTank.name = response.username;
@@ -13,7 +19,6 @@ window.onload = function(){
         tankToControl = userTank;
         tanks.push(userTank);
 
-<<<<<<< HEAD
         ticker = setInterval(function () {
             if (gameStarted) {
                 checkInput(userTank);
@@ -25,39 +30,3 @@ window.onload = function(){
         }, 16.67);
     });
 };
-=======
-    window.onload = function(){
-        // var testTank = clone(defaultTank);
-        // testTank.x = 1050;
-        // tanks.push(testTank);
-        console.log("testing branch");
-        var userTank = clone(defaultTank);
-        socket.on('join game', function(response){
-            var response = JSON.parse(response);
-
-            if(response.status == 'error'){
-                //send back to main menu if no username entered
-                window.location.replace('/');
-            }
-
-            clientId = response.userId;
-            userTank.userId = response.userId;
-            userTank.name = response.username;
-            userTank.team = response.userId;
-            tankToControl = userTank;
-            tanks.push(userTank);
-
-            ticker = setInterval(function () {
-                if (gameStarted) {
-                    checkInput(userTank);
-                    moveBullets();
-                    updateTanks();
-
-                }
-                draw();
-            }, 16.67);
-        });
-    }
-
-
->>>>>>> 6a861399be6f0a3c3942ff4a65e46abd82be73a6
